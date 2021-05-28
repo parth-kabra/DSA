e580f5d2c9f67e9a6d50608f50adf83886ddd37a
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-05-28 14:54:59 +0530

    Create 	 Longest Palindromic Substring.py

---
+++ LeetCode/Longest Palindromic Substring.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res=""
+        for i in range(len(s)):
+            for j in range(len(s),i,-1):
+                if(len(res) >= j-i):
+                    break
+                elif(s[i:j]==s[i:j][::-1]):
+                    res=s[i:j]
+        return res
