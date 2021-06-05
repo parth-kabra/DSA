7e6cb3f4864849bbb1bbf0030159cee0f91c27f8
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-05 12:33:26 +0530

    Create k largest elements.cpp

---
+++ GFG/k largest elements.cpp
@@ -0,0 +1,48 @@
+/*
+https://practice.geeksforgeeks.org/problems/k-largest-elements4206/1/?company[]=Google&company[]=Google&page=1&query=company[]Googlepage1company[]Google
+*/
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    // code here
+	    vector<int> res;
+	    sort(arr,arr+n,greater<int>());
+	    for(int i=0;i<k;i++){
+	        res.push_back(arr[i]);
+	    }
+	    return res;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr, n, k);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
