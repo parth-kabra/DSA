b384fe4ab2a9aa3e7fb4f87c18fa9e0136070fe2
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-05 20:13:14 +0530

    Create Josephus problem .cpp

---
+++ GFG/Josephus problem .cpp
@@ -0,0 +1,38 @@
+/*
+https://practice.geeksforgeeks.org/problems/josephus-problem/1/?category[]=Recursion&category[]=Recursion&page=1&query=category[]Recursionpage1category[]Recursion#
+*/
+
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int josephus(int n, int k)
+{
+   //Your code here
+    if (n == 1)
+        return 1;
+    else
+        return (josephus(n - 1, k) + k-1) % n + 1;
+}
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	scanf("%d", &t);//testcases
+	while(t--)
+	{
+		int n,k;
+		scanf("%d%d", &n, &k);//taking input n and k
+		
+		//calling josephus() function
+		printf("%d\n", josephus(n,k));
+	}
+	return 0;
+}  // } Driver Code Ends
