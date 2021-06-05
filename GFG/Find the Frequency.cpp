c241af56359663ed7954f5f5d854c9054a15b967
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-05 12:25:02 +0530

    Create Find the Frequency.cpp

---
+++ GFG/Find the Frequency.cpp
@@ -0,0 +1,50 @@
+/*
+https://practice.geeksforgeeks.org/problems/find-the-frequency/1/?company[]=Google&company[]=Google&page=1&query=company[]Googlepage1company[]Google
+*/
+
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*Function to find frequency of x
+* x : element whose frequency is to be found
+* v : input vector
+*/
+int findFrequency(vector<int> v, int x){
+    // Your code here
+    return count(v.begin(),v.end(),x);
+}
+
+// { Driver Code Starts.
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int N;
+	    cin >> N;
+	    
+	    // Declaring vector 
+	    vector<int> v;
+	        
+	    // Taking vector element input
+	    for(int i = 0;i<N;i++){
+	        int k;
+	        cin >> k;
+	        v.push_back(k);
+	    }
+	    
+	    // element whose frequency to be find
+	    int x;
+	    cin >> x;
+	    cout << findFrequency(v, x) << endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends
