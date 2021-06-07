0802290f5c42cc7f0e1fd0bd99b0daa8232c4e4d
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-07 17:56:29 +0530

    Create Print Linked List elements.cpp

---
+++ GFG/Print Linked List elements.cpp
@@ -0,0 +1,89 @@
+/*
+https://practice.geeksforgeeks.org/problems/print-linked-list-elements/1/?category[]=Linked%20List&category[]=Linked%20List&problemStatus=unsolved&difficulty[]=-1&difficulty[]=0&page=1&query=category[]Linked%20ListproblemStatusunsolveddifficulty[]-1difficulty[]0page1category[]Linked%20List
+*/
+
+// { Driver Code Starts
+#include <iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*start;
+
+void insert();
+
+ // } Driver Code Ends
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     Node *next;
+  }
+*/
+
+class Solution
+{
+    public:
+    void display(Node *n)
+    {
+    	while (n != NULL) {
+    		cout << n->data << " ";
+    		n = n->next;
+    	}
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+      start=NULL;
+      insert();
+      Solution ob;
+      ob.display(start);
+      cout<<endl;
+    }
+    return 0;
+
+}
+
+
+ void insert()
+ {
+     int n,value;
+     cin>>n;
+     struct Node *temp;
+     for(int i=0;i<n;i++)
+     {
+         cin>>value;
+         if(i==0)
+         {
+              start=(struct Node *) malloc( sizeof(struct Node) );
+              start->data=value;
+              start->next=NULL;
+              temp=start;
+              continue;
+         }
+         else
+         {
+             temp->next= (struct Node *) malloc( sizeof(struct Node) );
+             temp=temp->next;
+             temp->data=value;
+             temp->next=NULL;
+         }
+     }
+ }
+
+  // } Driver Code Ends
