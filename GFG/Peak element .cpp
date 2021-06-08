c6c35bfc73b21dfce616b8f8a9ec024659636d85
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-08 14:28:49 +0530

    Create Peak element .cpp

---
+++ GFG/Peak element .cpp
@@ -0,0 +1,69 @@
+/*
+https://practice.geeksforgeeks.org/problems/peak-element/1/?company[]=Amazon&company[]=Amazon&problemStatus=unsolved&problemType=functional&difficulty[]=0&page=1&sortBy=submissions&query=company[]AmazonproblemStatusunsolvedproblemTypefunctionaldifficulty[]0page1sortBysubmissionscompany[]Amazon#
+*/
+
+// { Driver Code Starts
+//Initial Template for C
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+int peakElement(int arr[], int n)
+{
+    if (n == 1) 
+      return 0;
+    if (arr[0] >= arr[1])
+        return 0;
+    if (arr[n - 1] >= arr[n - 2])
+        return n - 1;
+    for (int i = 1; i < n - 1; i++) {
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+            return i;
+    }
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		int n;
+		scanf("%d", &n);
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			scanf("%d", &a[i]);
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		
+		int A = peakElement(tmp,n);
+		
+		if(A<0 && A>=n)
+		    printf("0\n");
+		else
+		{
+		if(n==1 && A==0)
+		f=1;
+		else
+		if(A==0 && a[0]>=a[1])
+		f=1;
+		else if(A==n-1 && a[n-1]>=a[n-2])
+		f=1;
+		else if(a[A]>=a[A+1] && a[A]>= a[A-1])
+		f=1;
+		else
+		f=0;
+		printf("%d\n", f);
+		}
+		
+	}
+
+	return 0;
+}  // } Driver Code Ends
