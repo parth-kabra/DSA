a6439c6d9dbac85b213ef63c77146a6c728bd4a3
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-09 12:57:08 +0530

    Create Sort String.py

---
+++ GFG/Sort String.py
@@ -0,0 +1,8 @@
+"""
+https://practice.geeksforgeeks.org/problems/sort-string/0/?category[]=Sorting&category[]=Sorting&problemStatus=unsolved&page=1&query=category[]SortingproblemStatusunsolvedpage1category[]Sorting#
+"""
+
+#code
+for i in range(int(input())):
+    s=input()
+    print("".join(sorted(s)))Q
