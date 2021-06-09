b8e4fa23f4cae9a248f71b30b89327de63d240f8
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-09 21:13:39 +0530

    Create Mean.py

---
+++ GFG/Mean.py
@@ -0,0 +1,26 @@
+"""
+https://practice.geeksforgeeks.org/problems/mean0021/1/?category[]=Mathematical&category[]=Mathematical&page=1&query=category[]Mathematicalpage1category[]Mathematical#
+"""
+
+#User function Template for python3
+from statistics import mean
+class Solution:
+    def mean(self, N , A):
+        # code here 
+        return int(mean(A))
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        A = list(map(int,input().split()))
+        
+        ob = Solution()
+        print(ob.mean(N,A))
+# } Driver Code Ends
