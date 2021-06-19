660c74d1001389d132595f92606907ae6ef3ede8
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-19 17:18:19 +0530

    Create Sort the string in descending order.cpp

---
+++ GFG/Sort the string in descending order.cpp
@@ -0,0 +1,29 @@
+// https://practice.geeksforgeeks.org/problems/sort-the-string-in-descending-order3542/1/?category[]=Hash&category[]=Hash&problemStatus=unsolved&problemType=functional&page=3&query=category[]HashproblemStatusunsolvedproblemTypefunctionalpage3category[]Hash#
+
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+string ReverseSort(string str);
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+
+        string s;
+        cin >> s;
+        cout << ReverseSort(s) << endl;
+    }
+return 0;
+}
+// } Driver Code Ends
+
+
+
+string ReverseSort(string str){
+    //complete the function here
+    sort(str.begin(),str.end());
+    reverse(str.begin(),str.end());
+    return str;
+}
