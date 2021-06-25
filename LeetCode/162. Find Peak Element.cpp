a4ed46d88909f34e8ca221be355a76e32b140019
Parth Santosh Kabra <84372000+parth-kabra@users.noreply.github.com>
2021-06-25 19:26:46 +0530

    Create 162. Find Peak Element.cpp

---
+++ LeetCode/162. Find Peak Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        while(low < high){
+            int mid=(low+high)>>1;
+            if(nums[mid] > nums[mid+1]) high=mid;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
